aaad58b2a75e843716515f60915734a9fe1a5515
Zigrino <yulian.zigrino@gmail.com>
2022-02-09 17:59:47 -0500

    I don't fucking know what I'm doing

--- main.py
+++ main.py
@@ -11,8 +11,13 @@ class UI:
             for j in range(len(array[i])):
                window.grid(row =  i, column = j)
                
+class Logic:
+    def __init__(self):
+       pass 
+logic = Logic()
 ui = UI()
+
 
                
 if __name__ == "__main__":
-    pass
+    window.mainloop()
