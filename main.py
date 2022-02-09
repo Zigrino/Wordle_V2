31f1f2cb60868cef06f49fb9d3c596b5723a533f
Zigrino <yulian.zigrino@gmail.com>
2022-02-09 17:06:08 -0500

    added frames

--- main.py
+++ main.py
@@ -4,7 +4,15 @@ window = tk.Tk()
 class UI:
     def __init__(self):
         self.answerFrame = tk.LabelFrame(text = "Revealed")
+        self.wordFrame = tk.LabelFrame(text = "Input words here")
+        self.buttonFrame = tk.Frame()
     def draw_array(self, array):
         for i in range(len(array)):
             for j in range(len(array[i])):
                window.grid(row =  i, column = j)
+               
+ui = UI()
+
+               
+if __name__ == "__main__":
+    pass
