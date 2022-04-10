15b22bacc5b707c149bc3b7201d38b17f982cf87
Zigrino <yulian.zigrino@gmail.com>
2022-04-09 23:02:36 -0400

    fixed word counts i think

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ class UI:
         self.keyboardFrame = tk.LabelFrame(text =  "Letters")
         self.keyboardstr = "qwertyuiopasdfghjklzxcvbnm"
         self.keyboard = []
+        #sdfff
         self.errorFrame = tk.Frame()
         self.errorLabel = tk.Label(self.errorFrame)
         for i in self.keyboardstr:
@@ -69,10 +70,11 @@ class Logic:
         self.NumberOfGuessedWords = 0  
         self.keyboardstr = "qwertyuiopasdfghjklzxcvbnm"
         self.wordcounts = {"a": 0, "b": 0, "c": 0, "d": 0, "e": 0, "f": 0, "g": 0, "h": 0, "i": 0, "j": 0, "k": 0, "l": 0, "m": 0, "n": 0, "o": 0, "p": 0, "q": 0, "r": 0, "s": 0, "t": 0, "u": 0, "v": 0, "w": 0, "x": 0, "y": 0, "z": 0,}
-        self.timescounted = {"a": 0, "b": 0, "c": 0, "d": 0, "e": 0, "f": 0, "g": 0, "h": 0, "i": 0, "j": 0, "k": 0, "l": 0, "m": 0, "n": 0, "o": 0, "p": 0, "q": 0, "r": 0, "s": 0, "t": 0, "u": 0, "v": 0, "w": 0, "x": 0, "y": 0, "z": 0,}
-        self.winword = "bluff"
+        self.timesinword = {"a": 0, "b": 0, "c": 0, "d": 0, "e": 0, "f": 0, "g": 0, "h": 0, "i": 0, "j": 0, "k": 0, "l": 0, "m": 0, "n": 0, "o": 0, "p": 0, "q": 0, "r": 0, "s": 0, "t": 0, "u": 0, "v": 0, "w": 0, "x": 0, "y": 0, "z": 0,}
+        #self.winword = "bluff"
         for i in self.winword:
             self.wordcounts[i] += 1
+        print(self.wordcounts)
     def checkWord(self, word, error):
 
         if len(word)!=5:
@@ -122,13 +124,39 @@ class Logic:
     def proccessWord(self, inputWord, outputArray, keyboard, error):
         print("proccessing word")
         word = inputWord.get()
-
+        color_list = ["grey", "grey", "grey", "grey", "grey"]
+        timescounted = {"a": 0, "b": 0, "c": 0, "d": 0, "e": 0, "f": 0, "g": 0, "h": 0, "i": 0, "j": 0, "k": 0, "l": 0, "m": 0, "n": 0, "o": 0, "p": 0, "q": 0, "r": 0, "s": 0, "t": 0, "u": 0, "v": 0, "w": 0, "x": 0, "y": 0, "z": 0,}
         if self.checkWord(word, error):
             inputWord.destroy()
+            '''
             for i in range(len(outputArray[self.NumberOfGuessedWords])):
                 outputArray[self.NumberOfGuessedWords][i].config(text = word[i], bg = self.generateColor(word[i], i, keyboard, word), fg = "blue")
                 outputArray[self.NumberOfGuessedWords][i].grid(row = self.NumberOfGuessedWords, column = i)
             self.NumberOfGuessedWords += 1
+            '''
+             
+            for i in word:
+                self.timesinword[i]+=1
+
+
+            for i in range(5):
+                if self.winword[i] == word[i]:
+                    color_list[i] = "green"
+                    timescounted[word[i]]+=1
+            for i in range(5):
+                if word[i] in self.winword and timescounted[word[i]] < self.wordcounts[word[i]] and color_list[i] == "grey":
+                    color_list[i] = "yellow"    
+                
+            print(timescounted)
+            
+                    
+            for i in range(len(outputArray[self.NumberOfGuessedWords])):
+                outputArray[self.NumberOfGuessedWords][i].config(text = word[i], bg = color_list[i], fg = "blue")
+                outputArray[self.NumberOfGuessedWords][i].grid(row = self.NumberOfGuessedWords, column = i)
+            self.NumberOfGuessedWords += 1
+
+                    
+                    
         if word == self.winword:
             tk.messagebox.showinfo("", "You Win! :D")
     def cheat(self, e):
